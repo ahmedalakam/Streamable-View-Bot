b2b04aa02fd3f3025111832c9554ee83a218a887
Aytu <83882209+aytuuuuuuuuu@users.noreply.github.com>
2021-05-25 12:59:23 -0700

    Update streamable-v4.py

--- streamable-v4.py
+++ streamable-v4.py
@@ -133,6 +133,11 @@ class AutoPlayF:
 
     def FAutoPlay(self):
         while True:
+            if self.added >= self.amount:
+                os.system("taskkill /F /IM chrome.exe")
+                print(Fore.GREEN+"You Have Received The Requested Amount of Views!, Thank you for using Aytu Crypt.")
+                sleep(.3)
+                sys.exit()
             options = webdriver.ChromeOptions()
             options.add_argument("--log-level=OFF")
             options.add_argument("--headless")
@@ -167,11 +172,6 @@ class AutoPlayF:
                 sleep(.5)
                 driver.quit()
                 sleep(3)
-        if self.added >= self.amount:
-            os.system("taskkill /F /IM chrome.exe")
-            print(Fore.GREEN+"You Have Received The Requested Amount of Views!, Thank you for using Aytu Crypt.")
-            sleep(.3)
-            sys.exit()
             
     def start_title(self):
 
