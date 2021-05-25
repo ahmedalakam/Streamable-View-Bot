601fc6aad7ce452be76b8c23954f5000ac497e2a
Aytu <83882209+aytuuuuuuuuu@users.noreply.github.com>
2021-05-25 11:33:40 -0700

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Streamable Modules Installer
+python -m pip install -r requirements.txt
+echo Finished!
+pause
---
+++ Launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Aytu Streamable Viewbot Launcher
+taskkill /F /IM chrome.exe
+python streamable-v4.py
+pause
